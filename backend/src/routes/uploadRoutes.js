8d7cab4622577c93e615b1143356ae48b5c3ac80
Vishvarajsinh Zala <vishvarajsinhzala001@gmail.com>
2026-08-13 10:32:06 +0530

    Working share on X

--- backend/src/routes/uploadRoutes.js
+++ backend/src/routes/uploadRoutes.js
@@ -1,11 +1,9 @@
 import express from 'express';
 import { uploadImage } from '../controllers/uploadController.js';
 import { upload } from '../middleware/uploadMiddleware.js';
-import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/', protect, upload.single('image'), uploadImage);
+router.post('/', upload.single('image'), uploadImage);
 
 export default router;
-
